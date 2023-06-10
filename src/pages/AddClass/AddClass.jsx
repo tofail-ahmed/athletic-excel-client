21e766c2c971d2ea0aa9604e31e7b65487ccda51
Tofail Ahmed Sayem <atofail50@gmail.com>
2023-06-10 23:37:11 +0600

    add class doen by axios

--- src/pages/AddClass/AddClass.jsx
+++ src/pages/AddClass/AddClass.jsx
@@ -1,9 +1,35 @@
+
+import axios from 'axios';
 import React from 'react';
 
 const AddClass = () => {
+      const classData = {
+            name: 'Class Name',
+            image: 'class_image.jpg',
+            seatsAvailable: 10,
+            price: 50,
+            students: 2,
+            instructor: {
+                  name: 'Instructor Name',
+                  image: 'instructor_image.jpg',
+                  email: 'instructor@example.com'
+            }
+      };
+      const HandlePost = () => {
+            axios.post('http://localhost:5000/classes', classData)
+                  .then(res => {
+                        console.log(classData);
+                        console.log('Class created successfully', res.data);
+                        if(res.data.insertedId){
+                              alert('class created successfully')
+                        }
+
+                  })
+      }
       return (
             <div>
                   <h1 className='text-5xl'>This is add class</h1>
+                  <button onClick={HandlePost}>add class</button>
             </div>
       );
 };
